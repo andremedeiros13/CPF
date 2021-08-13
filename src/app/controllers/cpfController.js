e848953907a1cb7bc126686677294c68e1f1d54e
andremedeiros13 <andre13medeiros@gmail.com>
2021-08-13 14:47:04 -0300

    refatoring services

--- src/app/controllers/cpfController.js
+++ src/app/controllers/cpfController.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const cpfModel = require("../models/cpfModel");
-const { isValidCPF } = require("../services/services")
+const { findAll, findOne, create, remove } = require("../services/services")
 
 // mongoose.set('useFindAndModify', false);
 
@@ -9,88 +9,42 @@ module.exports = {
     async findAllCPFs(req, res) {
 
         try {
-            
-            const cpfs = await cpfModel.find({});
-            const result = cpfs.map((res)=>{                
-                const newRes = [];
-                newRes.push(res.cpf);
-                newRes.push(res.created_at);
-                console.log(newRes);
-                return newRes;
-            })            
-            return res.json(result);
-
-
+            const result = await findAll();
+            console.log(result);
+            return res.status(200).json(result);
         } catch (error) {
             res.status(500).send(error);
         }
-    },
-    
+    },    
     async checkCPF(req, res) {
-        try {
-            const { cpf } = req.params;
+        try {                  
+            const result = await findOne(req.params.cpf);            
+            console.log(result);
+            return res.status(200).json(result);            
 
-            //Verifica se o CPF digitado é valido        
-            if (!isValidCPF(cpf)) {
-                return res.status(404).json({ type: "InvalidCpfException", message: "CPF is not valid." });
-            }
-            //Verifica se existe o CPF na base de dados
-            const findCPF = await cpfModel.findOne({ cpf: cpf });
-            console.log(findCPF);
-            if (!findCPF) {
-                return res.status(404).json({ type: "NotFoundCpfException", message: "CPF not found." });
-            }
-            const {created_at} = findCPF;
-            return res.json({cpf, created_at});
-
-        } catch (error) {
-            return res.status(500).send(error);
+        } catch (error) {            
+            return res.status(500).json(error.message);
         }
 
     },
 
     async addCPF(req, res) {
         try {
-            const newCpf = new cpfModel(req.body);
-            const { cpf } = newCpf;
-
-            //Verificando se o CPF já está cadastrado na base
-            const existsCPF = await cpfModel.findOne({ cpf: cpf });
-            if (existsCPF != null) {
-                return res.status(404).json({ type: "ExistsCpfException", message: "CPF already registered." });
-            }
-
-            //Verifica se o CPF informado é valido!
-            if (!isValidCPF(cpf)) {
-                return res.status(404).json({ type: "InvalidCpfException", message: "CPF is not valid." });
-            }
-
-            await newCpf.save();
-            return res.json(newCpf);
-
-
+            const newResult = await create(req.body)            
+            return res.status(200).json(newResult);
         } catch (error) {
-            return res.status(500).json(error);
+            return res.status(500).json(error.message);
         }
     },
     
     async removeCPF(req, res) {
         try {
-            //Verifica se o CPF digitado é valido  
-            const { cpf } = req.params;
-            if (!isValidCPF(cpf)) {
-                return res.status(404).json({ type: "InvalidCpfException", message: "CPF is not valid." });
-            }
-            const findCPF = await cpfModel.findOneAndDelete({ cpf: req.params.cpf });
-            //Verifica se o CPF existe na base.
-            if (!findCPF) {
-                return res.status(404).json({ type: "NotFoundCpfException", message: "CPF not found." });
-            }
 
-            return res.status(200).json({message: "CPF successfully removed."});
+            const result = await remove(req.params.cpf)                        
+            return res.status(200).json(result);
 
         } catch (error) {
-            return res.status(500).send(error);
+            return res.status(500).send(error.message);
         }
 
     }
--- src/app/services/services.js
+++ src/app/services/services.js
@@ -1,39 +1,113 @@
-module.exports = {
-     isValidCPF(cpf) {
-        if (typeof cpf !== "string") return false
-        cpf = cpf.replace(/[\s.-]*/igm, '')
-        if (
-            !cpf ||
-            cpf.length != 11 ||
-            cpf == "00000000000" ||
-            cpf == "11111111111" ||
-            cpf == "22222222222" ||
-            cpf == "33333333333" ||
-            cpf == "44444444444" ||
-            cpf == "55555555555" ||
-            cpf == "66666666666" ||
-            cpf == "77777777777" ||
-            cpf == "88888888888" ||
-            cpf == "99999999999" 
-        ) {
-            return false
-        }
-        var soma = 0
-        var resto
-        for (var i = 1; i <= 9; i++) 
-            soma = soma + parseInt(cpf.substring(i-1, i)) * (11 - i)
-        resto = (soma * 10) % 11
-        if ((resto == 10) || (resto == 11))  resto = 0
-        if (resto != parseInt(cpf.substring(9, 10)) ) return false
-        soma = 0
-        for (var i = 1; i <= 10; i++) 
-            soma = soma + parseInt(cpf.substring(i-1, i)) * (12 - i)
-        resto = (soma * 10) % 11
-        if ((resto == 10) || (resto == 11))  resto = 0
-        if (resto != parseInt(cpf.substring(10, 11) ) ) return false
-        return true
-    
-        //fonte:Função para validar CPF
-        //https://gist.github.com/joaohcrangel/8bd48bcc40b9db63bef7201143303937
+const cpfModel = require("../models/cpfModel");
+function isValidCPF(cpf) {
+    if (typeof cpf !== "string") return false
+    cpf = cpf.replace(/[\s.-]*/igm, '')
+    if (
+        !cpf ||
+        cpf.length != 11 ||
+        cpf == "00000000000" ||
+        cpf == "11111111111" ||
+        cpf == "22222222222" ||
+        cpf == "33333333333" ||
+        cpf == "44444444444" ||
+        cpf == "55555555555" ||
+        cpf == "66666666666" ||
+        cpf == "77777777777" ||
+        cpf == "88888888888" ||
+        cpf == "99999999999" 
+    ) {
+        return false
     }
+    var soma = 0
+    var resto
+    for (var i = 1; i <= 9; i++) 
+        soma = soma + parseInt(cpf.substring(i-1, i)) * (11 - i)
+    resto = (soma * 10) % 11
+    if ((resto == 10) || (resto == 11))  resto = 0
+    if (resto != parseInt(cpf.substring(9, 10)) ) return false
+    soma = 0
+    for (var i = 1; i <= 10; i++) 
+        soma = soma + parseInt(cpf.substring(i-1, i)) * (12 - i)
+    resto = (soma * 10) % 11
+    if ((resto == 10) || (resto == 11))  resto = 0
+    if (resto != parseInt(cpf.substring(10, 11) ) ) return false
+    return true
+
+    //fonte:Função para validar CPF
+    //https://gist.github.com/joaohcrangel/8bd48bcc40b9db63bef7201143303937
+}
+
+module.exports = {     
+    async findAll() {
+        try {
+            
+            const cpfs = await cpfModel.find({});                        
+            const resultCPF = cpfs.map((res)=>{                
+                const newRes = [];
+                newRes.push(res.cpf);
+                newRes.push(res.created_at);                
+                return newRes;
+            
+        })       
+        
+            return  resultCPF;
+
+
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    },
+    async findOne(cpf) {        
+            //Verifica se o CPF digitado é valido
+            if (!isValidCPF(cpf)) {                
+                throw new Error("CPF is not valid.");    
+            }
+
+            //Verifica se existe o CPF na base de dados
+            const findCPF = await cpfModel.findOne({ cpf: cpf });  
+            if (!findCPF) {
+                throw new Error("CPF not found.");                    
+            }
+            const {created_at} = findCPF;
+            const resultCPF = [];
+            resultCPF.push(cpf);
+            resultCPF.push(created_at);                        
+            return resultCPF;
+
+    },
+    async create(body) {        
+            const newCpf = new cpfModel(body);
+            console.log(body);
+            const { cpf } = newCpf;
+
+            //Verificando se o CPF já está cadastrado na base
+            const existsCPF = await cpfModel.findOne({ cpf: cpf });
+            if (existsCPF != null) {
+                throw new Error("CPF already registered.");                        
+            }
+
+            //Verifica se o CPF informado é valido!
+            if (!isValidCPF(cpf)) {
+                throw new Error("CPF is not valid.");                     
+            }
+            await newCpf.save();
+            return newCpf;        
+    },
+
+    async remove(cpf) {        
+            //Verifica se o CPF digitado é valido  
+            if (!isValidCPF(cpf)) {
+                throw new Error("CPF is not valid.");                 
+            }
+            const findCPF = await cpfModel.findOneAndDelete({ cpf: cpf });
+            //Verifica se o CPF existe na base.
+            if (!findCPF) {
+                throw new Error("CPF not found.");                 
+            }
+            const message = "CPF successfully removed.";            
+
+            return message;       
+
+    }
+    
 }
